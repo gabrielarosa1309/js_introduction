701cee4ae2918afbdae69f0827ec901ec6e2e8ee
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-10-18 08:18:09 -0300

    Calculadora finalizada

--- imc/script.js
+++ imc/script.js
@@ -8,9 +8,11 @@
 //7.Renderizar o conteúdo no HTML (tabela)
 //8.Botão de limpar os registros
 
-function calcular() {
-    event.preventDefault();
-    
+const arrPessoas = []; //array vazio
+
+function calcular(e) {
+    e.preventDefault(); //capturar o evento de submit do formulário (para)
+
     //Pegar os dados do form
     //Validar se estão preenchidos
 
@@ -18,10 +20,10 @@ function calcular() {
     const altura = parseFloat(document.getElementById('altura').value);
     const peso = parseFloat(document.getElementById('peso').value);
 
-    if(isNaN(altura) || isNaN(peso) || nome.length == 0){
-        alert ("É necessário preencher os números corretamente!")
-        return;
-    } 
+    if (isNaN(altura) || isNaN(peso) || nome.length == 0) {
+        alert("É necessário preencher os números corretamente!")
+        return; //aqui é colocado um return pq assim ele para a funçao e n prossegue com os próximos passos (cálculos de imc)
+    }
 
     //Calcular imc
     const imc = calcularImc(altura, peso);
@@ -29,26 +31,56 @@ function calcular() {
 
     console.log(nome);
     console.log(altura);
-    console.log(peso);  
+    console.log(peso);
     console.log(imc);
+    console.log(situacao);
+
+    //Gerar objeto
+    const pessoa = { nome, altura, peso, imc, situacao }; //objeto literal
+
+    arrPessoas.push(pessoa);
+
+    //
+    listarPessoas();
 }
 
-function calcularImc(altura, peso){
+function calcularImc(altura, peso) {
     return peso / Math.pow(altura, 2);
 }
 
-function retornaSituacao(imc){
+function retornaSituacao(imc) {
     if (imc < 18.5) {
-        resultadoImc = "Magreza severa"
+        return "Magreza severa"
     } else if (imc <= 24.99) {
-        resultadoImc = "Peso normal"
+        return "Peso normal"
     } else if (imc <= 29.99) {
-        resultadoImc = "Sobrepeso"
+        return "Sobrepeso"
     } else if (imc <= 34.99) {
-        resultadoImc = "Obesidade I"
+        return "Obesidade I"
     } else if (imc <= 39.99) {
-        resultadoImc = "Obesidade II"
+        return "Obesidade II"
     } else {
         return "MÓRBIDO!!!"
     }
 }
+
+function listarPessoas() {
+    console.log(arrPessoas);
+
+    let template = "";
+
+    arrPessoas.forEach((p) => {
+        template += `
+            <tr>
+                <td>${p.nome}</td>
+                <td>${p.altura}</td>
+                <td>${p.peso}</td>
+                <td>${p.imc.toFixed(2)}</td>
+                <td>${p.situacao}</td>
+            </tr>`;
+    });
+
+    document.getElementById('corpo-tabela').innerHTML = template;
+}
+
+//escopo = visibilidade/abrangencia
